5c0b350d8bdbc2d75fa1597d26d66e466e2e34e6
Your Name <you@example.com>
2021-03-31 17:07:49 +0200

    2wpts

---
+++ 2wpts.cpp
@@ -0,0 +1,129 @@
+// #include <stdio.h>
+#define WITHOUT_NUMPY
+#include "gradient_planner_lib3.h"
+#include <string>
+#include <iostream>
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
+using namespace std;
+using namespace Eigen;
+
+Vector4d x0_backup, x_backup, flex;
+
+float k1=0.4;
+float k4=0.3;
+float c1=0.4;
+float c4=0.3;
+float t1=0.2;
+float t4=0.2;
+
+float side(Vector4d& a, Vector4d& b){
+	float mul=sin(a(2))*(b(0)-a(0))+cos(a(2))*(b(1)-a(1));
+	float curv=a(3);
+	std::cout << mul << "\n";
+	std::cout << curv << "\n";
+	std::cout << mul*curv << "\n";
+	if (mul*curv>=0) return fabs(curv);
+	return -fabs(curv);
+}
+
+void bezier(Vector4d& start_point, Vector4d& end_point){
+    Vector4d P2, P3;
+    float d=sqrt(pow(start_point(0)-end_point(0),2)+pow(start_point(1)-end_point(1),2));
+    float s1=side(start_point,end_point);
+    P2(0)=start_point(0)+cos(start_point(2)+start_point(3)*t1)*d*(k1-c1*s1);
+    P2(1)=start_point(1)+sin(start_point(2)+start_point(3)*t1)*d*(k1-c1*s1);
+    float s4=-side(end_point,start_point);
+    P3(0)=end_point(0)-cos(end_point(2)-end_point(3)*t4)*d*(k4-c4*s4);
+    P3(1)=end_point(1)-sin(end_point(2)-end_point(3)*t4)*d*(k4-c4*s4);
+
+    int steps=d/0.15;
+
+    std::vector<double> x2(steps+1);
+    std::vector<double> y2(steps+1);
+
+    for(int i=0;i<=steps;i++) {
+        float t=float(i)/float(steps);
+        x2[i] = (1 - t) * (1 - t) * (1 - t) * (1 - t) * start_point(0) + 
+		4 * t * (1 - t) * (1 - t) * (1-t) * P2(0) +
+		6 * t * t * (1 - t) * (1 - t) * flex(0) +
+		4 * t * t * t * (1-t) * P3(0) +
+                    t * t * t * t * end_point(0);
+	y2[i] = (1 - t) * (1 - t) * (1 - t) * (1 - t) * start_point(1) + 
+                4 * t * (1 - t) * (1 - t) * (1-t) * P2(1) +
+                6 * t * t * (1 - t) * (1 - t) * flex(1) +
+                4 * t * t * t * (1-t) * P3(1) +
+                    t * t * t * t * end_point(1);
+
+
+
+        //y2[i] = (1 - t) * (1 - t) * (1 - t) * start_point(1) + 3 * t * (1 - t) * (1 - t) * P2(1) + 3 * t * t * (1 - t) * P3(1) +
+          //             t * t * t * end_point(1);
+
+    }
+
+    plt::named_plot("Bezier", x2,y2);
+}
+
+
+void compute_trajectory_caller(params& p, Vector4d& start_point, Vector4d& end_point){
+    MatrixXd part1;
+    x0_backup = start_point;
+    x_backup = end_point;
+    std::vector<Vector4d> my_wpts;
+    my_wpts.push_back(x0_backup);
+    my_wpts.push_back(flex);
+    my_wpts.push_back(x_backup);
+    //cout << x2[steps]; 
+    //cout << x_backup; 
+    //cout << p.v; 
+    part1 = compute_trajectory(p, my_wpts);
+
+    std::vector<double> x(part1.cols());
+    std::vector<double> y(part1.cols());
+    for(size_t i = 0; i < x.size(); i++){
+	x[i]=part1(0, i);
+	y[i]=part1(1, i);
+    }
+
+    plt::named_plot("Gradient", x,y);
+    //cout << part1;
+}
+
+int main (int argc, char** argv){
+        params p;
+        Vector4d start_pt;
+        Vector4d end_pt;
+        MatrixXd ret;
+
+        p.T=0.1;
+        p.v=2.5;
+        c1=stod(argv[1]);
+        c4=stod(argv[2]);
+        k1=stod(argv[3]);
+        k4=stod(argv[4]);
+	p.Wye=0;
+    	p.Wyaw=0;
+    	p.Wc=1;
+    	p.Wdc=0.1;
+        start_pt(0)=stod(argv[5]);
+        start_pt(1)=stod(argv[6]);
+        start_pt(2)=stod(argv[7]);
+        start_pt(3)=stod(argv[8]);
+        end_pt(0)=stod(argv[9]);
+        end_pt(1)=stod(argv[10]);
+        end_pt(2)=stod(argv[11]);
+        end_pt(3)=stod(argv[12]);
+        flex(0)=stod(argv[13]);
+        flex(1)=stod(argv[14]);
+	
+        bezier(start_pt, end_pt);
+        compute_trajectory_caller(p, start_pt, end_pt);
+	plt::axis("equal");
+	plt::legend();
+        plt::show();
+	//plt::save("bezier.pdf");
+        return 0;
+}
+
--- main.cpp
+++ main.cpp
@@ -9,40 +9,95 @@ namespace plt = matplotlibcpp;
 using namespace std;
 using namespace Eigen;
 
-Vector4d x0_backup, x_backup;
+Vector4d x0_backup, x_backup, flex;
+float dif=0.15;
 
-float k1=0.4;
-float k4=0.3;
+struct WEIGHTS{
+	float k1, k4,c1,c4,t1,t4,dif,dif2;
+};
 
-void compute_trajectory_caller(params& p, Vector4d& start_point, Vector4d& end_point){
-    Vector4d P2, P3;
+class Bezier{
+float side(Vector4d& a, Vector4d& b){
+        float mul=sin(a(2))*(b(0)-a(0))+cos(a(2))*(b(1)-a(1));
+        float curv=a(3);
+        std::cout << mul << "\n";
+        std::cout << curv << "\n";
+        std::cout << mul*curv << "\n";
+        if (mul*curv>=0) return fabs(curv);
+        return -fabs(curv);
+}
+
+float angle(Vector4d& a, Vector4d& b){
+        return atan2(b(1)-a(1),b(0)-a(0));
+}
+WEIGHTS w;
+Vector4d P2, P3;
+float alpha, beta;
+std::vector<double> x2;
+std::vector<double> y2;
+
+void comp_curve(Vector4d& start_point, Vector4d& end_point){
     float d=sqrt(pow(start_point(0)-end_point(0),2)+pow(start_point(1)-end_point(1),2));
-    P2(0)=start_point(0)+cos(start_point(2))*d*k1;
-    P2(1)=start_point(1)+sin(start_point(2))*d*k1;
-    P3(0)=end_point(0)-cos(end_point(2))*d*k4;
-    P3(1)=end_point(1)-sin(end_point(2))*d*k4;
-
     int steps=d/0.15;
 
-    std::vector<double> x2(steps+1);
-    std::vector<double> y2(steps+1);
-
-    for(int i=1;i<=steps;i++) {
+    for(int i=0;i<=steps;i++) {
         float t=float(i)/float(steps);
-        x2[i] = (1 - t) * (1 - t) * (1 - t) * start_point(0) + 3 * t * (1 - t) * (1 - t) * P2(0) + 3 * t * t * (1 - t) * P3(0) +
+        float x = (1 - t) * (1 - t) * (1 - t) * start_point(0) + 3 * t * (1 - t) * (1 - t) * P2(0) + 3 * t * t * (1 - t) * P3(0) +
                        t * t * t * end_point(0);
-        y2[i] = (1 - t) * (1 - t) * (1 - t) * start_point(1) + 3 * t * (1 - t) * (1 - t) * P2(1) + 3 * t * t * (1 - t) * P3(1) +
+        float y = (1 - t) * (1 - t) * (1 - t) * start_point(1) + 3 * t * (1 - t) * (1 - t) * P2(1) + 3 * t * t * (1 - t) * P3(1) +
                        t * t * t * end_point(1);
+	x2.push_back(x);
+	y2.push_back(y);
 
     }
+}
+void comp_ctrl_pts(Vector4d& start_point, Vector4d& end_point){
+    float d=sqrt(pow(start_point(0)-end_point(0),2)+pow(start_point(1)-end_point(1),2));
+    float s1=side(start_point,end_point);
 
+    //in pi range	
+    float thetad = fabs(in_pi_range(start_point(2)-end_point(2)));
+
+    P2(0)=start_point(0)+cos(alpha)*d*(w.k1-w.c1*s1+thetad*w.dif);
+    P2(1)=start_point(1)+sin(alpha)*d*(w.k1-w.c1*s1+thetad*w.dif);
+    float s4=-side(end_point,start_point);
+    P3(0)=end_point(0)-cos(beta)*d*(w.k4-w.c4*s4*s4+thetad*w.dif2);
+    P3(1)=end_point(1)-sin(beta)*d*(w.k4-w.c4*s4*s4+thetad*w.dif2);
+}
+public:
+void bezier(Vector4d& start_point, Vector4d& end_point){
+    w={0.5,0.4,0.2,0,0.2,0.2,0.15,0};
+    alpha=start_point(2)+start_point(3)*w.t1;
+    beta=end_point(2)-end_point(3)*w.t4;
+    comp_ctrl_pts(start_point, end_point);
+    comp_curve(start_point, end_point);
+    plt::named_plot("Bezier", x2,y2);
+}
+
+void bezier(Vector4d& start_point, Vector4d& end_point, Vector4d& flex){
+    w={0.55,0.4,0.2,0,0.25,0.25,0.15,0};
+    alpha=start_point(2)+start_point(3)*w.t1;
+    beta=angle(start_point,end_point)-start_point(3)*w.t1+end_point(3)*w.t4;
+    comp_ctrl_pts(start_point, flex);
+    comp_curve(start_point, flex);
+    w={0.5,0.4,0.2,0,0.2,0.2,0,0.15};
+    alpha=beta;
+    beta=end_point(2)-end_point(3)*w.t4;
+    comp_ctrl_pts(flex, end_point);
+    comp_curve(flex, end_point);
+    plt::named_plot("Bezier", x2,y2);
+}
+};
+
+void compute_trajectory_caller(params& p, Vector4d& start_point, Vector4d& end_point){
     MatrixXd part1;
     x0_backup = start_point;
     x_backup = end_point;
     std::vector<Vector4d> my_wpts;
     my_wpts.push_back(x0_backup);
+    my_wpts.push_back(flex);
     my_wpts.push_back(x_backup);
-    cout << x2[steps]; 
+    //cout << x2[steps]; 
     //cout << x_backup; 
     //cout << p.v; 
     part1 = compute_trajectory(p, my_wpts);
@@ -54,9 +109,7 @@ void compute_trajectory_caller(params& p, Vector4d& start_point, Vector4d& end_p
 	y[i]=part1(1, i);
     }
 
-    plt::plot(x,y);
-    plt::plot(x2,y2);
-    plt::show();
+    plt::named_plot("Gradient", x,y);
     //cout << part1;
 }
 
@@ -66,24 +119,35 @@ int main (int argc, char** argv){
         Vector4d end_pt;
         MatrixXd ret;
 
-        p.T=stod(argv[1]);
-        p.v=stod(argv[2]);
-        k1=stod(argv[3]);
-        k4=stod(argv[4]);
+        p.T=0.1;
+        p.v=2.5;
 	p.Wye=0;
     	p.Wyaw=0;
     	p.Wc=1;
     	p.Wdc=0.1;
-        start_pt(0)=stod(argv[5]);
-        start_pt(1)=stod(argv[6]);
-        start_pt(2)=stod(argv[7]);
-        start_pt(3)=stod(argv[8]);
-        end_pt(0)=stod(argv[9]);
-        end_pt(1)=stod(argv[10]);
-        end_pt(2)=stod(argv[11]);
-        end_pt(3)=stod(argv[12]);
-
-        compute_trajectory_caller(p, start_pt, end_pt);
+        start_pt(0)=stod(argv[1]);
+        start_pt(1)=stod(argv[2]);
+        start_pt(2)=stod(argv[3]);
+        start_pt(3)=stod(argv[4]);
+        end_pt(0)=stod(argv[5]);
+        end_pt(1)=stod(argv[6]);
+        end_pt(2)=stod(argv[7]);
+        end_pt(3)=stod(argv[8]);
+        flex(0)=stod(argv[9]);
+        flex(1)=stod(argv[10]);
+        dif=stod(argv[11]);
+        flex(2)=end_pt(2);
+        flex(3)=end_pt(3);
+	
+	Bezier b;	
+        b.bezier(start_pt, end_pt, flex);
+	
+        
+	compute_trajectory_caller(p, start_pt, end_pt);
+	plt::axis("equal");
+	plt::legend();
+        plt::show();
+	//plt::save("bezier.pdf");
         return 0;
 }
 
